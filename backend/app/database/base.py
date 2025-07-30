624ea2da3eac685caec62ac414d4156e41f93ca6
Pedrohenrc <pirriquecsousa@gmail.com>
2025-07-30 20:37:13 -0300

    Add SQLAlchemy base and engine setup in base.py

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,1 +1,11 @@
 #SQLalchemy base + engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+DATABASE_URL = "sqlink"
+
+engine = create_engine(
+    DATABASE_URL,
+)
