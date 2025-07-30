73b525f8e0606ee383cb67fe1a4ed1c8f8ba4326
pedroromulo3 <pedro.rsoares14@gmail.com>
2025-07-30 14:50:24 -0300

    add miniaeronave e companhiaAerea

--- backend/app/models/voo.py
+++ backend/app/models/voo.py
@@ -1,1 +1,49 @@
 #Voo, Miniaeronave, Companhia Aérea
+
+class MiniAeronave:
+    """Objeto da composição dentro de Voo."""
+    def __init__(self, modelo: str, capacidade: int):
+        self.modelo = modelo 
+        self.capacidade = capacidade 
+    
+    def resumo_voo(self):
+        return f"{self.modelo} com capacidade para {self.capacidade} passageiros. "
+    
+
+class CompanhiaAerea:
+    """Agrupa seus voos (has-a)."""
+    def __init__(self, nome: str):
+        nome = nome.strip()
+        
+        if len(nome) < 3:
+            raise ValueError("o nome da companhia deve ter pelo menos 3 letras.")
+        
+        self._nome = nome
+        self._voos = []
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, novo_nome: str):
+        novo_nome = novo_nome.strip()
+
+        if len(novo_nome) < 3:
+            raise ValueError ("o nome da companhia deve ter pelo menos 3 letras.")
+        
+        self._nome = novo_nome
+       
+    def adicionar_voo(self, voo):
+        self._voos.append(voo)
+        
+    def buscar_voo(self, numero: str):
+        for voo in self._voos:
+            if voo.numero == numero:
+                return voo
+        return None
+        
+    def listar_voos(self):
+        for voo in self._voos:
+            print(voo)
+
