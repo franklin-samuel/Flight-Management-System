8e5bfa80d981d424007c738b39a78a3ac3cd45cd
Pedrohenrc <pirriquecsousa@gmail.com>
2025-07-30 10:14:34 -0300

    Add Voo class and rename requirements file
    Introduces the Voo class with basic attributes and a method stub in voo.py. Also renames 'requiriments.txt' to 'requirements.txt' for correct dependency management.

--- backend/app/models/voo.py
+++ backend/app/models/voo.py
@@ -1,1 +1,14 @@
 #Voo, Miniaeronave, Companhia Aérea
+
+class Voo:
+    def __init__(self, numero_voo, origem, destino, aeronave):
+        self.numero_voo = numero_voo
+        self.origem = origem
+        self.destino = destino
+        self.aeronave = aeronave
+        self.passageiros = {}
+        self.tripulação = {}
+        # implementar database nas listas 
+
+    def adicionar_passageiro(self, passageiro, numero_voo):
+
