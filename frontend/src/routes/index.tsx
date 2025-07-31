805fc0113ebe50dcf8e0c287c8b6e2b8ed91cc70
Samuel Franklin <samuelfranklinff@gmail.com>
2025-07-31 11:16:57 -0300

    Companhia Context
    create Companhia Context to envolve the application with data, to use the data required of API in any place of application

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { CompanhiaProvider } from '../context/companhiaContext';
 import { Navbar } from '../components/layout/NavBar';
 import Home from '../pages/Home';
 import { Voos } from '../pages/Voos';
@@ -8,6 +9,7 @@ import { Companhias } from '../pages/Companhias';
 import { CompanhiaDetalhes } from '../pages/CompanhiaDetalhes';
 import { Relatorios } from '../pages/Relatorios';
 import { VooDetalhes } from '../pages/VoosDetalhes';
+import Passageiro from '../pages/PassageiroDetalhes';
 
 export default function RoutesApp() {
     return (
@@ -15,16 +17,19 @@ export default function RoutesApp() {
             <div className='min-h-screen bg-gray-50'>
                 <Navbar />
                 <main className='container mx-auto px-4 py-8'>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/Voos" element={<Voos />} />
-                        <Route path="/voos/:id" element={<VooDetalhes />} />
-                        <Route path="/Funcionarios" element={<Funcionarios />}/>
-                        <Route path="/Auditoria" element={<Auditoria />} />
-                        <Route path="/Companhias" element={<Companhias />} />
-                        <Route path="/companhias/:id" element={<CompanhiaDetalhes />} />
-                        <Route path="/Relatorios" element={<Relatorios />} />
-                    </Routes>
+                    <CompanhiaProvider>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/Voos" element={<Voos />} />
+                            <Route path="/voos/:id" element={<VooDetalhes />} />
+                            <Route path="passageiro/:id" element={<Passageiro />} />
+                            <Route path="/Funcionarios" element={<Funcionarios />}/>
+                            <Route path="/Auditoria" element={<Auditoria />} />
+                            <Route path="/Companhias" element={<Companhias />} />
+                            <Route path="/companhias/:id" element={<CompanhiaDetalhes />} />
+                            <Route path="/Relatorios" element={<Relatorios />} />
+                        </Routes>
+                    </CompanhiaProvider>
                 </main>
             </div>
         </BrowserRouter>
