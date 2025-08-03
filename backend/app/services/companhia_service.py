ee78eb9181dcda3dac1c8b401fcc227cc9b603e3
Pedrohenrc <pirriquecsousa@gmail.com>
2025-08-02 21:24:59 -0300

    Add criar_companhia function to companhia service

--- backend/app/services/companhia_service.py
+++ backend/app/services/companhia_service.py
@@ -1,3 +1,10 @@
 from sqlalchemy.orm import Session
-from app.database.models import CompanhiaAerea as companhiaDB
+from app.database.models import CompanhiaAerea as CompanhiaDB
+from app.services.mappers.companhia_mapper import companhia_from_db
+def criar_companhia(db: Session, nome: str):
+    nova = CompanhiaDB(nome=nome)
+    db.add(nova)
+    db.commit()
+    db.refresh(nova)
+    return companhia_from_db(nova)
 
