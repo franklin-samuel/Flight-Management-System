3af1a559e1f820f96d8de1cfd03b76a01dcabad8
Pedrohenrc <pirriquecsousa@gmail.com>
2025-08-02 21:21:06 -0300

    Add companhia mapper and service foundation

---
+++ backend/app/services/mappers/companhia_mapper.py
@@ -0,0 +1,7 @@
+from app.database.models import Companhia as CompanhiaDB
+from app.models.voo import CompanhiaAerea
+
+def companhia_from_db(db_model: CompanhiaDB) -> CompanhiaAerea:
+    return CompanhiaAerea(
+        nome=db_model.nome
+    )
