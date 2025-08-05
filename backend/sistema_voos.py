05476634818386cbdec2def60a66f22e290570fb
Samuel Franklin <samuelfranklinff@gmail.com>
2025-08-04 21:48:22 -0300

    imports manually tests

--- backend/sistema_voos.py
+++ backend/sistema_voos.py
@@ -1,1 +1,8 @@
-#testes manuais caso necessário
+from app.services.funcionario_service import FuncionarioService;
+from app.services.passageiro_service import PassageiroService;
+from app.services.companhia_service import CompanhiaAerea;
+from app.services.voo_service import VooService;
+from app.services.aeronave_services import AeronaveService;
+from app.services.auditoria_service import Auditor;
+import pytest;
+
