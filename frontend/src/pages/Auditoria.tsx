08799ae3d11f99ffe02d693ac623c061b0a10cef
Samuel Franklin <samuelfranklinff@gmail.com>
2025-09-26 17:35:21 -0300

    Delete frontend directory
    frontend directory is not using

--- frontend/src/pages/Auditoria.tsx
+++
@@ -1,5 +0,0 @@
-export default function Auditoria() {
-    return (
-        <h1>Página de Auditoria</h1>
-    )
-}
--- frontend/src/pages/PassageiroDetalhes.tsx
+++
@@ -1,7 +0,0 @@
-export default function Passageiro() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
